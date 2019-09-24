06e766c4bc90c7155ccf5a6729639b2b2ab52e3d
Bienbenido Angeles <bangeles@Bienbenidos-MacBook-Pro.local>
2019-09-24 15:52:00 -0400

    added Xcode project

---
+++ swiftUIDemo/swiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIDemo
+//
+//  Created by Bienbenido Angeles on 9/24/19.
+//  Copyright © 2019 Bienbenido Angeles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
